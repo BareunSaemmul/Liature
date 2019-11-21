d3c034cb27888906e8dfc7a3ceecf65bd9c303cb
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-11-21 16:49:25 +0900

    binary 파일 이동

--- AI/test.py
+++ AI/test.py
@@ -1,9 +1,16 @@
-from kor2vec import Kor2Vec
-kor2vec = Kor2Vec.load("../model/path")
+# from kor2vec import Kor2Vec
+# kor2vec = Kor2Vec.load("../model/path")
 
-kor2vec.embedding("안녕 아이오아이야 나는 클로바에서 왔어")
+# kor2vec.embedding("안녕 아이오아이야 나는 클로바에서 왔어")
 
-kor2vec.embedding("나는 도라에몽이라고 해 반가워", numpy=True)
+# kor2vec.embedding("나는 도라에몽이라고 해 반가워", numpy=True)
 
-input = kor2vec.to_seqs(["안녕 나는 뽀로로라고 해", "만나서 반가워 뽀로로"], seq_len=4)
-kor2vec.forward(input)
+# input = kor2vec.to_seqs(["안녕 나는 뽀로로라고 해", "만나서 반가워 뽀로로"], seq_len=4)
+# kor2vec.forward(input)
+
+import gensim
+
+model = gensim.models.Word2Vec.load('../../ko.bin')
+# result = model.wv.most_similar("땅 미친듯이 흔들림 진짜 뒤질거같아")
+print(model.wv.most_similar("강아지"))
+# print(result)
