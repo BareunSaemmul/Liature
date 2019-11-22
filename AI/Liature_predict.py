434129c821b85318a79177945b56357a7e11c959
Nam-Seung-Woo <45066060+Nam-SW@users.noreply.github.com>
2019-11-22 13:20:58 +0900

    일단은 완성

--- AI/Liature_predict.py
+++ AI/Liature_predict.py
@@ -5,11 +5,19 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 import pickle
 from tensorflow.keras.models import load_model
+import crawling
+import time
 
 
 class Liature_model:
     def __init__(self):
         self.model = load_model('Liature.h5')
+        self.okt = Okt()
+        with open('sentences.txt', 'rb') as f:
+            X = pickle.load(f)
+        max_words = 3000
+        self.t = Tokenizer(num_words=max_words) # 상위 3000개의 단어만 보존
+        self.t.fit_on_texts(X)
 
     def preprocessing(self, input_data):
         '''
@@ -24,54 +32,61 @@ class Liature_model:
 
         stopwords=['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다'] # 불용어
         # print(input_data)
-        okt = Okt()
+        
         x = []
         for sentence in input_data:
             temp_X = []
-            temp_X = okt.morphs(sentence, stem=True) # 토큰화
+            temp_X = self.okt.morphs(sentence, stem=True) # 토큰화
             temp_X = [word for word in temp_X if not word in stopwords] # 불용어 제거
             x.append(temp_X)
-        with open('sentences.txt', 'rb') as f:
-            X = pickle.load(f)
+        
 
         # x.append(x)
         # print(x)
-        max_words = 3000
-        t = Tokenizer(num_words=max_words) # 상위 3000개의 단어만 보존
-        t.fit_on_texts(X)
-        x = t.texts_to_sequences(x) # 단어 벡터 길이에 맞게 추가 또는 삭제
+        x = self.t.texts_to_sequences(x) # 단어 벡터 길이에 맞게 추가 또는 삭제
 
         # for i in range(len(x)):
         #     if x[i] == []:
         #         x[i] = 0
         #     else:
         #         x[i] = x[i][0]
-        print(x)
+        # print(x)
 
         x = pad_sequences(x, maxlen=15)
-        print(x)
+        # print(x)
         return x
     
-    def predict(self, *input_data):
+    def predict(self, input_data):
         '''
-        예측할 데이터를 ,단위로 입력. 입력한 개수만큼 예측 위험도를 리스트로 반환
+        예측할 데이터를 리스트로 입력. 입력한 개수만큼 예측 위험도를 리스트로 반환
         '''
-        preprocessed_data = self.preprocessing(list(input_data))
+        preprocessed_data = self.preprocessing(input_data)
         # print(preprocessed_data)
-        pred = self.model.predict(preprocessed_data)[0]
-        # print(pred)
-        answer = []
-        for i in pred:
-            print((-i).argsort())
-            answer.append((-i).argsort()[0]+1)
+        pred = []
+        for i in preprocessed_data:
+            pred.append(self.model.predict(np.array([i])))
+        # answer = []
+        for i in range(len(pred)):
+            # print((-i).argsort())
+            pred[i] = (-pred[i]).argsort()[0][0]+1
 
-        return answer
+        return pred
+
+    def search(self, keyword):
+        url = 'https://search.naver.com/search.naver?where=realtime&sm=tab_rea&query=' + keyword + '&ie=utf8&best=0'
+        # while True:
+        data = crawling.search(url)
+        pred = self.predict(data)
+        print(pred)
+        # time.sleep(10)
+        
 
 
 model = Liature_model()
 # print(model.predict('포항 지진 안내도 없고 소리나고 흔들렸어요'))
-model.predict('포항 지진 안내도 없고 소리나고 흔들렸어요.', 
-                '방금 포항 지진났는데 이제 아예 문자도 안올리네', 
-                '방금 포항 지진났는데 왜 안 뜨나요ㅠㅠ 한참동안 안 그러다가 갑자기 그래서 진짜 놀랐네요', 
-                '포항 방금 지진 맞죠? 재난문자 아직도 안오네요', 
-                '포항 방금 지진 발생ㅠㅠ')
+# model.predict('포항 지진 안내도 없고 소리나고 흔들렸어요.', 
+#                 '방금 포항 지진났는데 이제 아예 문자도 안올리네', 
+#                 '방금 포항 지진났는데 왜 안 뜨나요ㅠㅠ 한참동안 안 그러다가 갑자기 그래서 진짜 놀랐네요', 
+#                 '포항 방금 지진 맞죠? 재난문자 아직도 안오네요', 
+#                 '포항 방금 지진 발생ㅠㅠ')
+model.search('지진')
--- AI/crawling.py
+++ AI/crawling.py
@@ -1,21 +1,37 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 def get_url(search):
     url = 'https://search.naver.com/search.naver?where=realtime&sm=tab_rea&query=검색어&ie=utf8&best=0'.replace('검색어', search)
     return url
 
+def search(url):
 
-chromedriver = 'C:\\ProgramData\\Anaconda3\\chromedriver.exe' # 윈도우 
-driver = webdriver.Chrome(chromedriver)
 
-driver.get(get_url(input('검색어 입력: ')))
+    chromedriver = 'C:\\ProgramData\\Anaconda3\\chromedriver.exe' # 윈도우 
+    driver = webdriver.Chrome(chromedriver)
 
-text = driver.find_elements_by_class_name('txt_link')
+    driver.get(url)
 
-for i in range(5):
-    print(text[i].text,end='\n\n')
+    load_button = driver.find_element_by_class_name('_moreBtn')
+    for i in range(10):
+        load_button.click()
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(0.2)
+
+    text = driver.find_elements_by_class_name('txt_link')
+
+    word_list = []
+
+    for i in range(30):
+        # print(text[i].text)
+        word_list.append(text[i].text)
+    # time.sleep(5)
+    return word_list
 
 # driver.quit()
 
 # print(get_url('포항 지진'))
+
+# print(search(get_url('지진')))
